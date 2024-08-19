5c922c48d3929c98ba7f51725df689f0e828944e
Vinicius64 <oliveira.vinicius64@gmail.com>
2024-08-19 20:47:24 -0300

    chore: projeto inicial do aplicativo de views

---
+++ app/src/main/java/com/vin/pdms5/views/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.vin.pdms5.views
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.vin.pdms5.views.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var amb: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        amb = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(amb.root)
+
+
+    }
+}
